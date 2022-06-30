ee3f4772256357938e34295e04d05fe3ca819f54
MkMonir <khanmonir1335@gmail.com>
2022-06-30 22:14:34 +0600

    basic crud operation

---
+++ api/controllers/handlerFactory.js
@@ -0,0 +1,77 @@
+import catchAsync from './../utils/catchAsync';
+import AppError from './../utils/appError';
+import APIFeatures from './../utils/apiFeatures';
+
+export const deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
+
+export const updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: doc,
+    });
+  });
+
+export const createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: doc,
+    });
+  });
+
+export const getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findById(req.params.id);
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: doc,
+    });
+  });
+
+export const getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const features = new APIFeatures(Model.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    // const doc = await features.query.explain();
+    const doc = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      result: doc.length,
+      data: doc,
+    });
+  });
