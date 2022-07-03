49c739553f8b0b455bd7470494ecd254f248046c
MkMonir <khanmonir1335@gmail.com>
2022-07-04 00:46:15 +0600

    add routes middlewares

--- api/app.js
+++ api/app.js
@@ -1,25 +1,41 @@
 import express from 'express';
+import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import helmet from 'helmet';
 import dotenv from 'dotenv';
-
-const app = express();
+import cors from 'cors';
+import globalErrorHandler from './controllers/errorController';
+import AppError from './utils/AppError';
+import authRouter from './routes/authRoutes';
+import userRouter from './routes/userRoutes';
+import productRouter from './routes/productRoutes';
+import orderRouter from './routes/orderRoutes';
+import cartRouter from './routes/cartRoutes';
 
 dotenv.config();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
+
 app.use(cors());
 
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-    crossOriginEmbedderPolicy: false,
-  })
-);
+// Middlewares
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/orders', orderRouter);
+app.use('/api/v1/carts', cartRouter);
 
-// app.use('/');
+// Global error handling middleware
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+app.use(globalErrorHandler);
 
 export default app;
