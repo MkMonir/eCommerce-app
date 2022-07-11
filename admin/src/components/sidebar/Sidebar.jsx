ca39464d06f08887789d22d5a492e9d4e8d8b743
MkMonir <khanmonir1335@gmail.com>
2022-07-11 22:37:33 +0600

    siderbar component

---
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,121 @@
+import { Link, useLocation } from 'react-router-dom';
+import './sidebar.css';
+import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  PermIdentity,
+  PlayCircleOutline,
+  List,
+  BarChart,
+  MailOutline,
+  DynamicFeed,
+  ChatBubbleOutline,
+  WorkOutline,
+  Report,
+} from '@material-ui/icons';
+
+export default function Sidebar() {
+  const location = useLocation();
+
+  const pathMatchRoute = (route) => {
+    if (route === location.pathname) return true;
+  };
+
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <Link to="/" className="link">
+              <li className={pathMatchRoute('/') ? 'sidebarListItemActive' : 'sidebarListItem'}>
+                <LineStyle className="sidebarIcon" />
+                Home
+              </li>
+            </Link>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUp className="sidebarIcon" />
+              Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <Link to="/users" className="link">
+              <li
+                className={pathMatchRoute('/users') ? 'sidebarListItemActive' : 'sidebarListItem'}
+              >
+                <PermIdentity className="sidebarIcon" />
+                Users
+              </li>
+            </Link>
+            <Link to="/products" className="link">
+              <li
+                className={
+                  pathMatchRoute('/products') ? 'sidebarListItemActive' : 'sidebarListItem'
+                }
+              >
+                <PlayCircleOutline className="sidebarIcon" />
+                Products
+              </li>
+            </Link>
+            <Link to="/transactions" className="link">
+              <li
+                className={
+                  pathMatchRoute('/transactions') ? 'sidebarListItemActive' : 'sidebarListItem'
+                }
+              >
+                <List className="sidebarIcon" />
+                Transactions
+              </li>
+            </Link>
+            <li className="sidebarListItem">
+              <BarChart className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <MailOutline className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeed className="sidebarIcon" />
+              Feedback
+            </li>
+            <li className="sidebarListItem">
+              <ChatBubbleOutline className="sidebarIcon" />
+              Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <WorkOutline className="sidebarIcon" />
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <Report className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
