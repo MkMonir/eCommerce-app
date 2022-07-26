974ba0178c0ab12194150402af2f26fc991bf221
MkMonir <khanmonir1335@gmail.com>
2022-07-26 15:30:00 +0600

    loader component

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,7 @@
+export default function ShowDetail() {
+  return (
+    <div className="tw-container text-center fa-6x loader center h-96 w-full  flex justify-center items-center">
+      <i className="fas fa-spinner fa-pulse"></i>
+    </div>
+  );
+}
